15e5127ec0ec401b3717ac3ccb64eddeb73de2c7
klaus-dieter <klaus-dieter.gundermann@web.de>
2015-05-12 23:40:55 +0200

    Add Location model

---
+++ db/migrate/20150202150000_create_locations.rb
@@ -0,0 +1,10 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.string  :code,   null: false
+      t.string  :city,   null: false
+
+      t.timestamps       null: false
+    end
+  end
+end
