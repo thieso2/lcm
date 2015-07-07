f0d87af9cfc21160125a33eb3e940c5cfaf03b0d
klaus-dieter <klaus-dieter.gundermann@web.de>
2015-07-07 22:48:04 +0200

    move import_xxx classes to lib

--- lib/import_events.rb
+++ lib/import_events.rb
@@ -4,49 +4,58 @@ class ImportEvents
 
     MAPPING = {
       "eID" => "eid",
-      "eName" => "shortname",
+      "eName" => "setshortname",
       "eBeschreibung" => "title",
-      "eOrt" => "location"
+      "eOrt" => "city"
     }
-    
+
     def self.read(file)
-      self.new file
+      me = self.new file
+      me.read_events
     end
-    
+
     def initialize(file)
       @file = file
-      read_events
     end
-    
-    private
+
     def read_events
+      msg = []
       rows = XlsxImport.read @file.path, SHEET_EVENTS
-      ActiveRecord::Base.transaction do        
+      ActiveRecord::Base.transaction do
         ActiveRecord::Base.logger.silence do
-        rows.each do |row|
-          next if row["eID"].blank?
-          import_row row
+          rows.each do |row|
+            next if row["eID"].blank?
+            msg << import_row(row)
+          end
         end
       end
-      end
+      return msg
     end
-    
+
+    private
     def import_row(row)
       c = Event.new
       MAPPING.each {|col,field|
-        c[field.to_sym] = row[col]
+        c.send(field + "=", row[col])
       }
-      
-      c.group_type_id = 1
+
+      #c.event_type_id = 1
       unless row["eJahr"].blank?
         c.startdate = row["eJahr"] + row["eZeitraum"][0..1] + "01"
       else
         c.startdate = "20000101"
       end
       c.event_state = :closed
-      
-      c.save!        
+
+      puts c
+
+      if c.save!
+        return c
+      end
+
+    rescue => e
+      puts "Error: #{e} #{e.backtrace}"
     end
-    
+
 
 end
