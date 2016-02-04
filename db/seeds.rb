2781fbfb0802f806ebbaf40dfd2d904bd948bcd2
klaus-dieter <klaus-dieter.gundermann@web.de>
2016-02-04 23:52:41 +0100

    Change db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 admin = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << admin.email
 
-# user = Person.create!(firstname: "Klaus", sex: :male )
-klaus = Person.create!(firstname: "Klaus", lastname: "Gundermann", sex: :male, country: "DE" )
+# klaus = Person.create!(id:1, firstname: "Klaus", lastname: "Gundermann", sex: :male, country: "DE" )
+admin_person = Person.find(1)
 
 reg_ger = Region.create! code: "GER", description: "Germany"
 
@@ -20,24 +20,48 @@ loc_muc = Location.create! code: "MUC", city: "München"
 loc_bln = Location.create! code: "BLN", city: "Berlin"
 loc_lnd = Location.create! code: "LND", city: "London"
 
-evt_lmf  = EventType.create! code: "LMF", description: "Landmark Forum"
-evt_fgk  = EventType.create! code: "FGK", description: "Fortgeschrittenenkurs"
-evt_wis  = EventType.create! code: "WIS", description: "Wisdom"
-evt_ilp  = EventType.create! code: "ILP", description: "Einführungsleiterprogramm"
+loc_han  = Location.create! code: "HAN", city: "Hanau"
+loc_lfr  = Location.create! code: "LFR", city: "London / Frankfurt"
+loc_ist  = Location.create! code: "IST", city: "Istanbul"
+loc_eur  = Location.create! code: "EUR", city: "Europa"
+loc_sin  = Location.create! code: "SIN", city: "Singapore"
 
-evt_exc  = EventType.create! code: "EXC", description: "Seminar Excellence"
-evt_cap  = EventType.create! code: "CAP", description: "Seminar Kommunikation: Zugang durch Kraft"
-evt_cpc  = EventType.create! code: "CPC", description: "Seminar Kommunikation: die Kraft zu Kreieren"
-evt_fia  = EventType.create! code: "FIA", description: "Seminar Forum in Aktion"
-evt_suc  = EventType.create! code: "SUC", description: "Seminar Erfolg"
-evt_ges  = EventType.create! code: "GES", description: "Seminar Geschwindigkeit"
-evt_mir  = EventType.create! code: "MIR", description: "Seminar Wunder"
-evt_inl  = EventType.create! code: "INL", description: "Seminar Ein Kreirtes Leben leben"
-evt_int  = EventType.create! code: "INT", description: "Seminar Integrität"
-evt_eff  = EventType.create! code: "EFF", description: "Seminar Effektivität"
-evt_mon  = EventType.create! code: "MON", description: "Seminar Geld"
-evt_rel  = EventType.create! code: "REL", description: "Seminar Beziehung"
-evt_blb  = EventType.create! code: "BLB", description: "Seminar Durchbrüche"
+
+
+evt_lmf   = EventType.create! code: "LMF", description: "Landmark Forum"
+evt_fgk   = EventType.create! code: "FGK", description: "Fortgeschrittenenkurs"
+evt_cap   = EventType.create! code: "CAP", description: "Kurs Kommunikation: Zugang durch Kraft"
+evt_cpc   = EventType.create! code: "CPC", description: "Kurs Kommunikation: die Kraft zu Kreieren"
+evt_mev   = EventType.create! code: "MEV", description: "Marken Event"
+
+evt_ilp   = EventType.create! code: "ILP", description: "Einführungsleiterprogramm"
+evt_csp   = EventType.create! code: "CSP", description: "Course Supverisor Programm"
+evt_tml   = EventType.create! code: "TML", description: "Team, Management und Leadership Programm"
+evt_sel   = EventType.create! code: "SEL", description: "Self Expression and Leadership Programm"
+evt_wis   = EventType.create! code: "WIS", description: "Wisdom Programm"
+
+evt_inl  = EventType.create! code: "SemINL", description: "Seminar Ein erfundenes Leben leben / Invented Live"
+evt_bex  = EventType.create! code: "SemBEX", description: "Seminar Aussergewöhnlich sein / Being Extraordinary"
+evt_byf  = EventType.create! code: "SemBYF", description: "Seminar Beyond Fitness"
+evt_blb  = EventType.create! code: "SemBLB", description: "Seminar Durchbrüche / Breakthroughs: Living Outside the Box"
+evt_ctm  = EventType.create! code: "SemCTM", description: "Seminar Wunder verursachen / Causing the Miraculous"
+evt_cpa  = EventType.create! code: "SemCPA", description: "Seminar Verpflichtung / Commitment: Pathway to Adventure"
+evt_cha  = EventType.create! code: "SemCHA", description: "Seminar Glücklichsein kreieren / Creating Happiness"
+evt_cld  = EventType.create! code: "SemCLD", description: "Seminar Kreativität / Creativity"
+evt_eff  = EventType.create! code: "SemEFF", description: "Seminar Effektivität / Effectiveness"
+evt_eiz  = EventType.create! code: "SemEIZ", description: "Seminar Excellence / Excellence in the Zone"
+evt_ibl  = EventType.create! code: "SemIBL", description: "Seminar Integrität / Integrity: The Bottom Line"
+evt_fia  = EventType.create! code: "SemFIA", description: "Seminar Forum in Aktion / Forum in Action"
+evt_lps  = EventType.create! code: "SemLPS", description: "Seminar Leidenschaftlich Leben / Living Passionately"
+evt_lpo  = EventType.create! code: "SemLPO", description: "Seminar Kraftvoll Leben / Living Powerfully"
+evt_mcf  = EventType.create! code: "SemMCF", description: "Seminar Geld / Money"
+evt_trs  = EventType.create! code: "SemTRS", description: "Seminar Beziehung / Relationships"
+evt_sai  = EventType.create! code: "SemSAI", description: "Seminar Sex und Intimität / Sex and Intimacy"
+evt_suc  = EventType.create! code: "SemSUC", description: "Seminar Erfolg / Success: The Power of Context"
+
+evt_suc  = EventType.create! code: "SemGES", description: "Seminar Geschwindigkeit"
+
+
 
 tt_assist = TeamType.create! code: "ASS", description: "Assistance Programm"
 tt_admins = TeamType.create! code: "ADM", description: "Administration"
@@ -55,10 +79,11 @@ admins = Team.create!(team_type: tt_admins, title: "Germany", startdate: "2015-0
 
 puts "Created Team: #{admins}"
 
-PersonTeamAssignment.create!(person: klaus, team: admins, team_role_type: rl_admin)
+PersonTeamAssignment.create!(person: admin_person, team: admins, team_role_type: rl_admin)
 
 # -----------------------------------------------------------------
 # Create Landmark Forum Frankfurt 2015/05
+=begin
 
 forum = Event.create!(event_type: evt_lmf, location: loc_frk,
                       startdate: "2015-05-01", event_state: "open")
@@ -82,6 +107,7 @@ assistance = Team.create!(team_type: tt_assist, region: reg_ger)
 puts "Created Event: #{assistance}"
 
 PersonTeamAssignment.create!(person: klaus, team: assistance, team_role_type: rl_ass, startdate: "2015-02-01", enddate: "2015-03-31")
+=end
 
 # -----------------------------------------------------------------
 # Reporting
