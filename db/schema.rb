c2a5a85638a15dca14dd2a985e514934ecb9b3f1
klaus-dieter <dbManager@lmassist.de>
2016-02-22 22:17:50 +0100

    Push updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,138 +13,122 @@
 
 ActiveRecord::Schema.define(version: 20151026205553) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "calls", force: :cascade do |t|
-    t.integer  "person_id"
+    t.integer  "person_id",  limit: 4
     t.datetime "date"
-    t.integer  "caller_id"
-    t.text     "info"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "caller_id",  limit: 4
+    t.text     "info",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   add_index "calls", ["caller_id"], name: "index_calls_on_caller_id", using: :btree
   add_index "calls", ["person_id"], name: "index_calls_on_person_id", using: :btree
 
   create_table "event_role_types", force: :cascade do |t|
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "event_types", force: :cascade do |t|
-    t.string   "code",        null: false
-    t.string   "description", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "code",        limit: 255, null: false
+    t.string   "description", limit: 255, null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "events", force: :cascade do |t|
-    t.integer  "event_type_id"
-    t.integer  "location_id"
-    t.integer  "eid"
-    t.string   "shortname"
-    t.string   "title"
+    t.integer  "event_type_id", limit: 4
+    t.integer  "location_id",   limit: 4
+    t.string   "shortname",     limit: 255
+    t.string   "title",         limit: 255
     t.date     "startdate"
     t.date     "enddate"
-    t.decimal  "baseprice",     precision: 8, scale: 2
-    t.integer  "event_state",                           default: 0, null: false
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
+    t.decimal  "baseprice",                 precision: 8, scale: 2
+    t.integer  "event_state",   limit: 4,                           default: 0, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "events", ["event_type_id"], name: "index_events_on_event_type_id", using: :btree
+  add_index "events", ["location_id"], name: "fk_rails_3d0bd29ec6", using: :btree
 
   create_table "import_jobs", force: :cascade do |t|
-    t.string   "original_filename"
-    t.string   "temp_filename"
+    t.string   "original_filename", limit: 255
+    t.string   "temp_filename",     limit: 255
     t.datetime "starttime"
     t.datetime "finishtime"
-    t.integer  "user_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.integer  "person_id",         limit: 4
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
-  add_index "import_jobs", ["user_id"], name: "index_import_jobs_on_user_id", using: :btree
+  add_index "import_jobs", ["person_id"], name: "index_import_jobs_on_person_id", using: :btree
 
   create_table "import_rows", force: :cascade do |t|
-    t.integer  "import_step_id"
-    t.integer  "row"
-    t.string   "rawdata"
-    t.string   "importdata"
-    t.string   "conflictdata"
-    t.integer  "state"
-    t.string   "message"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.integer  "import_step_id", limit: 4
+    t.integer  "row",            limit: 4
+    t.text     "rawdata",        limit: 65535
+    t.text     "importdata",     limit: 65535
+    t.text     "conflictdata",   limit: 65535
+    t.integer  "state",          limit: 4
+    t.string   "message",        limit: 255
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "import_steps", force: :cascade do |t|
-    t.integer  "import_job_id"
-    t.string   "description"
-    t.integer  "totalrows"
-    t.integer  "validrows"
-    t.integer  "errorrows"
-    t.string   "errortext"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer  "import_job_id", limit: 4
+    t.string   "description",   limit: 255
+    t.integer  "totalrows",     limit: 4
+    t.integer  "validrows",     limit: 4
+    t.integer  "errorrows",     limit: 4
+    t.string   "errortext",     limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "locations", force: :cascade do |t|
-    t.string   "code",       null: false
-    t.string   "city",       null: false
-    t.text     "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "code",       limit: 255,   null: false
+    t.string   "city",       limit: 255,   null: false
+    t.text     "address",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "people", force: :cascade do |t|
-    t.string   "email",                  default: ""
-    t.string   "encrypted_password",     default: ""
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "regions_id"
-    t.integer  "pid"
-    t.string   "firstname"
-    t.string   "lastname"
-    t.string   "sex"
-    t.integer  "salutation"
-    t.string   "country"
-    t.string   "region"
-    t.string   "zip"
-    t.string   "city"
-    t.string   "street"
-    t.string   "housenumber"
-    t.date     "birthday"
-    t.string   "phone_private"
-    t.string   "phone_work"
-    t.string   "phone_mobile"
-    t.string   "notes"
-    t.boolean  "do_not_contact"
-    t.integer  "access"
+    t.integer "region_id",      limit: 4
+    t.string  "firstname",      limit: 255
+    t.string  "lastname",       limit: 255
+    t.string  "callby",         limit: 255
+    t.string  "sex",            limit: 255
+    t.string  "title",          limit: 255
+    t.string  "country",        limit: 255
+    t.string  "region",         limit: 255
+    t.string  "zip",            limit: 255
+    t.string  "city",           limit: 255
+    t.string  "street",         limit: 255
+    t.string  "housenumber",    limit: 255
+    t.date    "birthday"
+    t.string  "email",          limit: 255
+    t.string  "state",          limit: 255
+    t.string  "date",           limit: 255
+    t.string  "phone_private",  limit: 255
+    t.string  "phone_work",     limit: 255
+    t.string  "phone_mobile",   limit: 255
+    t.text    "notes",          limit: 65535
+    t.boolean "do_not_contact"
   end
 
-  add_index "people", ["email"], name: "index_people_on_email", using: :btree
-  add_index "people", ["reset_password_token"], name: "index_people_on_reset_password_token", unique: true, using: :btree
-
   create_table "person_event_assignments", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "event_id"
-    t.integer  "event_role_type_id"
+    t.integer  "person_id",          limit: 4
+    t.integer  "event_id",           limit: 4
+    t.integer  "event_role_type_id", limit: 4
     t.date     "startdate"
     t.date     "enddate"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "person_event_assignments", ["event_id"], name: "index_person_event_assignments_on_event_id", using: :btree
@@ -152,13 +136,13 @@ ActiveRecord::Schema.define(version: 20151026205553) do
   add_index "person_event_assignments", ["person_id"], name: "index_person_event_assignments_on_person_id", using: :btree
 
   create_table "person_team_assignments", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "team_id"
-    t.integer  "team_role_type_id"
+    t.integer  "person_id",         limit: 4
+    t.integer  "team_id",           limit: 4
+    t.integer  "team_role_type_id", limit: 4
     t.date     "startdate"
     t.date     "enddate"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   add_index "person_team_assignments", ["person_id"], name: "index_person_team_assignments_on_person_id", using: :btree
@@ -166,57 +150,85 @@ ActiveRecord::Schema.define(version: 20151026205553) do
   add_index "person_team_assignments", ["team_role_type_id"], name: "index_person_team_assignments_on_team_role_type_id", using: :btree
 
   create_table "regions", force: :cascade do |t|
-    t.string   "code",        null: false
-    t.string   "description", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "code",        limit: 255, null: false
+    t.string   "description", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "reports", force: :cascade do |t|
-    t.string   "name"
-    t.string   "url_name"
-    t.string   "description"
+    t.string   "name",        limit: 255
+    t.string   "url_name",    limit: 255
+    t.string   "description", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "team_role_types", force: :cascade do |t|
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "team_types", force: :cascade do |t|
-    t.string   "code",        null: false
-    t.string   "description", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "code",        limit: 255, null: false
+    t.string   "description", limit: 255, null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "teams", force: :cascade do |t|
-    t.integer  "team_type_id"
-    t.integer  "region_id"
-    t.integer  "tid"
-    t.string   "shortname"
-    t.string   "title"
+    t.integer  "team_type_id", limit: 4
+    t.integer  "region_id",    limit: 4
+    t.string   "shortname",    limit: 255
+    t.string   "title",        limit: 255
     t.date     "startdate"
     t.date     "enddate"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   add_index "teams", ["team_type_id"], name: "index_teams_on_team_type_id", using: :btree
 
-  create_table "versions", force: :cascade do |t|
-    t.string   "item_type",      null: false
-    t.integer  "item_id",        null: false
-    t.string   "event",          null: false
-    t.string   "whodunnit"
-    t.text     "object"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: ""
+    t.string   "encrypted_password",     limit: 255, default: ""
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
-    t.text     "object_changes"
+    t.datetime "updated_at"
+    t.integer  "access",                 limit: 4
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",      limit: 255,   null: false
+    t.integer  "item_id",        limit: 4,     null: false
+    t.string   "event",          limit: 255,   null: false
+    t.string   "whodunnit",      limit: 255
+    t.text     "object",         limit: 65535
+    t.datetime "created_at"
+    t.text     "object_changes", limit: 65535
   end
 
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
+  add_foreign_key "calls", "people", column: "caller_id"
+  add_foreign_key "events", "event_types"
+  add_foreign_key "events", "locations"
+  add_foreign_key "person_event_assignments", "event_role_types"
+  add_foreign_key "person_event_assignments", "events"
+  add_foreign_key "person_event_assignments", "people"
+  add_foreign_key "person_team_assignments", "people"
+  add_foreign_key "person_team_assignments", "team_role_types"
+  add_foreign_key "person_team_assignments", "teams"
+  add_foreign_key "teams", "team_types"
 end
